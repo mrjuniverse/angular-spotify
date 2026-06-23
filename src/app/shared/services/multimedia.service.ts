83b664b732237b4b791fd7cb27e763000ee1babd
Jonathan <m4a72m4a72@gmail.com>
2026-06-23 11:48:40 -0400

    Hasta la clase 40

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -1,4 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.models';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +9,26 @@ export class MultimediaService {
 
   callback: EventEmitter<any> = new EventEmitter<any>()
 
-  constructor() {}
+  public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined)
+
+  public audio!: HTMLAudioElement
+
+  constructor() {
+    this.audio = new Audio()
+    this.trackInfo$.subscribe(responseOK =>{
+      if (responseOK) {
+        this.setAudio(responseOK)
+      }
+    })
+  }
+
+  private listenAllEvents(): void{}
+
+
+
+
+  public setAudio(track: TrackModel): void {
+    this.audio.src = track.url
+    this.audio.play()
+  }
 }
